19b9380877e77e049d863b2480971738c741be7d
ericcleptonsilva <ericcleptonsilva@gmail.com>
2021-03-30 22:33:58 -0300

    Create test int Array

--- src/main/kotlin/me/clept/api/collections/TEsteIntArray.kt
+++ src/main/kotlin/me/clept/api/collections/TEsteIntArray.kt
@@ -2,4 +2,29 @@ package me.clept.api.collections
 
 fun main() {
     val values = IntArray(5)
+
+    values[0] = 1
+    values[1] = 7
+    values[2] = 6
+    values[3] = 3
+    values[4] = 2
+
+    for (valor in values){
+        print(valor)
+
+    }
+
+    print("---")
+    values.forEach { valor -> print(valor)}
+    print("---")
+    for (index in values.indices){
+        print(values[index])
+    }
+    print("---")
+    values.sort()
+    for (valor in values){
+        print(valor)
+    }
+
+
 }
