cd65ad2fad810abad48b9cd24c42f3e11f075d81
ericcleptonsilva <ericcleptonsilva@gmail.com>
2021-03-31 16:24:57 -0300

    trabalando com DoubleArray

---
+++ src/main/kotlin/me/clept/api/collections/TEstDoubleArray.kt
@@ -0,0 +1,22 @@
+package me.clept.api.collections
+
+fun main() {
+    val salarios = DoubleArray(4)
+    salarios[0] = 1000.0
+    salarios[1] = 2000.0
+    salarios[2] = 5000.0
+    salarios[3] = 4000.0
+
+    salarios.forEach { println(it) }
+    println("-----------------")
+    salarios.forEachIndexed {index, salario ->
+        salarios[index] = salario * 1.1
+    }
+    salarios.forEach { println(it) }
+
+    println("-----------------")
+    val salarios2 = doubleArrayOf(1500.0, 1200.0, 5000.0)
+    salarios2.sort()
+    salarios2.forEach { println(it) }
+
+}
